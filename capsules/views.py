9cac8f5377cc82fa5985c975e22f9b28ab92ef1e
vasileios20 <vasilis_tsim@outlook.com>
2024-06-27 13:35:12 +0100

    feat: Add ImageList, VideoList, and GeminiMessageList views

--- capsules/views.py
+++ capsules/views.py
@@ -15,11 +15,40 @@ class CapsuleList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
+
 
 class CapsuleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Capsule.objects.all()
     serializer_class = CapsuleSerializer
     permission_classes = [IsAdminUserOrReadOnly]
-    
-    
+
+
+class ImageList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return Images.objects.filter(capsule=self.kwargs["pk"])
+
+    serializer_class = ImageSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class VideoList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return Videos.objects.filter(capsule=self.kwargs["pk"])
+
+    serializer_class = VideoSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class GeminiMessageList(generics.ListCreateAPIView):
+    # The gemini messages are associated with either an image or a video.
+    # The image or video is associated with a capsule.
+    # Therefore, the gemini messages can be fetched by the capsule id.
+
+    def get_queryset(self):
+        if "image_id" in self.kwargs:
+            return GeminiMessage.objects.filter(image=self.kwargs["image_id"])
+        elif "video_id" in self.kwargs:
+            return GeminiMessage.objects.filter(video=self.kwargs["video_id"])
+
+    serializer_class = GeminiMessageSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
