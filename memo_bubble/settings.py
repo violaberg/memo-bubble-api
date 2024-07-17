4688a1a03f41b6418c8d8c14e557f6e486ac7205
vasileios20 <vasilis_tsim@outlook.com>
2024-07-17 12:36:07 +0100

    chore: Update REST_FRAMEWORK authentication classes in settings.py

--- memo_bubble/settings.py
+++ memo_bubble/settings.py
@@ -22,13 +22,13 @@ if os.path.exists("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-#    "DEFAULT_AUTHENTICATION_CLASSES": [
-#        (
-#            "rest_framework.authentication.SessionAuthentication"
-#            if "DEV" in os.environ
-#            else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
-#        )
-#    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        (
+            "rest_framework.authentication.SessionAuthentication"
+            if "DEV" in os.environ
+            else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
+        )
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
     "DATETIME_FORMAT": "%d %b %Y",
