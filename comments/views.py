ab59b8dd5cc5f835ccd9cedfa542f9f3a457e8ca
vasileios20 <vasilis_tsim@outlook.com>
2024-08-09 18:01:39 +0100

    feat: Update CommentSerializer to include naturaltime for created_on and updated_on fields
    - Update recipient in notify.send to fix issue with ```"Notification.recipient" must be a "User" instance```
    - Add filters for comment and owner

--- comments/views.py
+++ comments/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, permissions
 from memo_bubble.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer
 from notifications.signals import notify
-from capsules.models import Capsule
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -17,12 +17,16 @@ class CommentList(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly
     ]
 
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['comment', 'owner']
+
     def perform_create(self, serializer):
         comment = serializer.save(owner=self.request.user)
         capsule = comment.capsule
+        owner = capsule.owner
         notify.send(
             self.request.user,
-            recipient=Capsule.owner,
+            recipient=owner,
             verb='commented on your capsule',
             target=capsule
         )
@@ -35,6 +39,3 @@ class CommentDetail(generics.RetrieveDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
